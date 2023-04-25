1b411c781fad6b2942ae40d65cf4382d91cfbdfa
Dhruv Bakshi <dhruvbakshi20@gmail.com>
2023-04-25 20:48:22 +0530

    app trials

--- application/app.py
+++ application/app.py
@@ -1,11 +1,63 @@
-from flask import Flask
+from flask import Flask, render_template, request, session
+import os
+from Store_Sales.utils import *
+from Store_Sales.constants import *
+from Store_Sales import logger
+from flask import Flask,request,app,jsonify,url_for,render_template
+from flask_sqlalchemy import SQLAlchemy
+import pickle
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
 
-app=Flask(__name__)
+from app_utils import *
 
-@app.route("/",methods=['GET','POST'])
-def index():
-    return "CI CD Pipeline established"
+secrets=read_yaml(SECRETS_FILE_PATH)
+model = pickle.load(open('application/model.pkl', 'rb'))
+transformer=pickle.load(open('application/model.pkl', 'rb'))
 
 
-if__name__=='__main__':
-    app.run(debug=True)
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.secret_key = secrets.FLASK_APP_KEY
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/last_predict')
+def last_predict():
+    return render_template('last_predict.html')
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    test_df=pd.read_csv(CLEAN_TEST_FILE_PATH)
+    item_type=list(test_df['Item_Type'].unique())
+    outlet_identifier=list(test_df['Outlet_Identifier'].unique())
+    if request.method=="POST":
+       #try:            
+            if request.form:
+                inputs=get_user_input_dataframe()
+                prediction = get_prediction(inputs)
+                print(prediction)
+                
+                return render_template('predict.html',item_type=item_type,
+                                       outlet_identifier=outlet_identifier)
+       
+       
+    else:
+        print('Method not post')
+        return render_template('predict.html',item_type=item_type,
+                                    outlet_identifier=outlet_identifier)
+
+if __name__=='__main__':
+    app.run(debug = True)
+
